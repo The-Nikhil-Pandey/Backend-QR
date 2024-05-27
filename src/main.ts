531609154672b41eb38509b912a6b6eb4617eedf
The-Nikhil <nikgarhwa7890@gmail.com>
2024-05-27 17:48:22 +0530

    new main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,40 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder().setVersion('1.0').addTag('cats').build();
+
+  app.enableCors({
+    origin: '*', // Replace with the actual origin of your Next.js app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Docs: SSLLP')
+    .setDescription('Seek Solution LLP')
+    .addBearerAuth(
+      { type: 'http', name: 'token', in: 'header' },
+      'authentication',
+    )
+    .setVersion('1.0')
+    .addTag('default')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(8085);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
+
+  await app.listen(process.env.PORT);
+  console.log(
+    `Server is running by main.ts on port : http://localhost:${process.env.PORT}`,
+  );
 }
 bootstrap();
